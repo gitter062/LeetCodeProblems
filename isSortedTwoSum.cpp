454e4a3ddc268d586daa3144dbd14d3ab57f1b11
Ishtiaque Zaman <123589194+gitter062@users.noreply.github.com>
2024-03-28 04:52:32 +0000

    redid the code

--- isSortedTwoSum.cpp
+++ isSortedTwoSum.cpp
@@ -1,36 +1,39 @@
 /*
-Example 2: Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise. This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
+Example 2: Given a sorted array of unique integers and a target integer,
+return true if there exists a pair of numbers that sum to target, false otherwise. 
+This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
 
 For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
 */
-
-// Use Two Pointer Technique, starting from the first and last elements respectively
+// use two pointer technique
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool isSortedTwoSum(vector<int> num, int target){
-    int rightPointer = num.size()-1;
-    int leftPointer = 0;
-    while(leftPointer < rightPointer){
-        if(num[leftPointer] + num[rightPointer] == target)
+bool isSortedTwoSum(vector<int>& arr, int target){
+    int left = 0, right = arr.size()-1;
+    while(left<right){
+        if (arr[right] + arr[left] == target)
             return true;
-        else if (num[leftPointer] + num[rightPointer] < target)
-            leftPointer++;
+        else if(arr[right] + arr[left] > target)
+            right--;
         else
-            rightPointer--;
-        
+            left++;
+
     }
+
     return false;
 }
 
-int main(){
-    vector <int> sortedArray {1, 2, 4, 6, 8, 9, 14, 15};
-    int targetSum = 5;
-
-    if(isSortedTwoSum(sortedArray, targetSum))
-        cout << "Two Sum Exists :)";
+int main(void){
+    vector<int> arr {1, 2, 4, 6, 8, 9, 14, 15};
+    int target  = 13;
+    if (isSortedTwoSum(arr, target)){
+        cout << "Is Two Sum";
+    }
     else
-        cout << "Two Sum does not Exist :(";
+        cout << "Not a two sum";
+
     return 0;
 }
