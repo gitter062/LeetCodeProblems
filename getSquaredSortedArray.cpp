b630932fdc1d0ddae788170f169b27fad1d537f3
Ishtiaque Zaman <123589194+gitter062@users.noreply.github.com>
2024-06-14 20:58:47 +0000

    updated getSquaredArray method

--- getSquaredSortedArray.cpp
+++ getSquaredSortedArray.cpp
@@ -1,27 +1,42 @@
+/*
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+*/
 #include <bits/stdc++.h>
+
 using namespace std;
 
-vector<int> squaredSortedArray(vector<int>& nums) {
-    int left = 0, right = nums.size()-1, index = nums.size()-1;
-    vector<int> answer(nums.size());
-    while(left<=right){
-        if(abs(nums[left]) < abs(nums[right])){
-            answer[index] = nums[right]*nums[right];
-            index--;
+vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> answer (nums.size(), 0);
+    int right = nums.size()-1, left = 0;
+    int index = answer.size()-1;
+
+    while(left <= right){
+        if(abs(nums[right]) > abs(nums[left])){
+            answer[index] = nums[right] * nums[right];
             right--;
         }
         else{
-            answer[index] = nums[left]*nums[left];
-            index--;
+            answer[index] = nums[left] * nums[left];
             left++;
         }
+        index--;
     }
     return answer;
 }
-int main(){
-    vector<int> sortedArray {-7,-3,2,3,11};
-    vector<int> result = squaredSortedArray(sortedArray);
-    for (int value:result)
-        cout<<value << " ";
+
+int main(void){
+    vector<int> nums = {-4,-1,0,3,10};
+    for(int i = 0; i < nums.size(); i++)
+        cout << sortedSquares(nums)[i] << endl;
     return 0;
 }
