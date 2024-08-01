c7d71942afeac9a25070546ca93aae042cb36f84
Ishtiaque Zaman <123589194+gitter062@users.noreply.github.com>
2024-08-01 23:21:40 +0000

    rewrote the valind palindrome problem with edge cases

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -11,30 +11,27 @@ A string is a palindrome if it reads the same forward as backward. That means, a
 using namespace std;
 
 
-bool isPalindrome(string str){
-    int left = 0;
-    int right = str.size()-1;
-
-    while(left < right){
-        while(left<right && !isalnum(str[left])){
+bool isPalindrome(string s){
+        int left = 0, right = s.size()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left]))
+                left++;
+            while(left<right && !isalnum(s[right]))
+                right--;
+            if(tolower(s[left]) != tolower(s[right])){
+                cout<< "Not a Palindrome!";
+                return false;
+            }
             left++;
-        }
-        while(left<right && !isalnum(str[right])){
             right--;
         }
-        if(islower(str[left]) != islower(str[right])){
-            return false;
-        }
-        left++;
-        right--;
-    }
-    cout << "this is a palindrome";
-    return true;
+        cout << "It's a Palindrome!";
+        return true;
 
 }
 
 int main(void){
-    string string1 = "racecar";
+    string string1 = "abba";
     cout << isPalindrome(string1);
     return 0;
 }
