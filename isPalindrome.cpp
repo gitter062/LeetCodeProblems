90679d01f4d074e5b5811844bb1912d8a47bf68c
Ishtiaque Zaman <123589194+gitter062@users.noreply.github.com>
2025-01-03 17:12:08 +0000

    redid valid palindrome problem

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -1,8 +1,28 @@
 /*
-Example 1: Given a string s, return true if it is a palindrome, false otherwise.
+Valid Palindrome
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
-A string is a palindrome if it reads the same forward as backward. That means, after reversing it, it is still the same string. For example: "abcdcba", or "racecar".
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
 */
+
+// leetcode link: https://leetcode.com/problems/valid-palindrome/submissions/1496493873
 
 // Use two pointer technique
 
@@ -10,29 +30,21 @@ A string is a palindrome if it reads the same forward as backward. That means, a
 
 using namespace std;
 
-
-bool isPalindrome(string s){
-        int left = 0, right = s.size()-1;
-        while(left<right){
-            while(left<right && !isalnum(s[left]))
+class Solution{
+    public:
+    bool isPalindrome(string s){
+        int right = s.size()-1, left = 0;
+        while(left < right){
+            while(left < right & !isalnum(s[left]))
                 left++;
-            while(left<right && !isalnum(s[right]))
+            while(left < right & !isalnum(s[right]))
                 right--;
-            if(tolower(s[left]) != tolower(s[right])){
-                cout<< "Not a Palindrome!";
+            if(tolower(s[left]) != tolower(s[right]))
                 return false;
-            }
-            left++;
-            right--;
+            
+            left++; right--;
         }
-        cout << "It's a Palindrome!";
         return true;
-
-}
-
-int main(void){
-    string string1 = "abba";
-    cout << isPalindrome(string1);
-    return 0;
-}
+    }
+};
 
