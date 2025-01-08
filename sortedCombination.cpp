6fe3d063986fc4c67e74dad05924c7439e53289e
Ishtiaque Zaman <123589194+gitter062@users.noreply.github.com>
2025-01-08 18:58:30 +0000

    redid

--- sortedCombination.cpp
+++ sortedCombination.cpp
@@ -3,45 +3,44 @@ Example 3: Given two sorted integer arrays arr1 and arr2, return a new array tha
 */
 
 // Use Two Pointer Technique, starting from the first elements of each array respectively
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> getSortedCombination(vector<int> arr1, vector<int> arr2){
-    vector<int> result;
-    int pointer1 = 0, pointer2 = 0;
+vector<int> getCombinedSort(vector<int> &arr1, vector<int> &arr2){
+    vector<int> arr;
+    int p1 = 0, p2 = 0;
 
-    while(pointer1<= arr1.size()-1 && pointer2 <= arr2.size()-1){
-        if(arr1[pointer1] < arr2[pointer2]){
-            result.push_back(arr1[pointer1]);
-            pointer1++;
+    while(p1 < arr1.size() & p2 < arr2.size()){
+        if(arr1[p1] < arr2[p2]){
+            arr.push_back(arr1[p1]);
+            p1++;
         }
         else{
-            result.push_back(arr2[pointer2]);
-            pointer2++;
+            arr.push_back(arr2[p2]);
+            p2++;
         }
     }
-    while(pointer1 <= arr1.size()-1){
-        result.push_back(arr1[pointer1]);
-        pointer1++;
+    while(p1>arr1.size()){
+        arr.push_back(arr1[p1]);
+        p1++;
     }
-    while(pointer2 <= arr2.size()-1){
-        result.push_back(arr2[pointer2]);
-        pointer2++;
+    while(p2>arr2.size()){
+        arr.push_back(arr2[p2]);
+        p2++;
     }
 
-    return result;
+    return arr;
 }
 
 
-int main(){
-    vector<int> arr1 {1,2,6,8};
-    vector<int> arr2 {5,6,7};
+int main(void){
+    
+    vector<int> arr1 = {1, 3, 5, 6};
+    vector<int> arr2 = {2, 4, 6};
 
-    vector<int> result = getSortedCombination(arr2, arr1);
+    vector<int> arr = getCombinedSort(arr1, arr2);
 
-    for (auto value: result)
-        cout << value << " ";
-    cout << endl;
     return 0;
 }
